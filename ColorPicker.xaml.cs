29edc51a7ba164bc5c7e086dcb46b1a378bbde03
IoRaa <ka_dandani@esi.dz>
2022-04-05 00:10:19 +0100

    added color picker

--- ColorPicker.xaml.cs
+++ ColorPicker.xaml.cs
@@ -20,11 +20,52 @@ namespace Projet2Cp
     /// </summary>
     public partial class ColorPicker : UserControl
     {
+        private Brush pickedColor { get; set; }
+        private Border hit; 
         public ColorPicker()
         {
             InitializeComponent();
 
+            Border BORD;
+            int k = 0; 
+            String[] colors  =new string[20] {"#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"};
+            for(int i = 0; i < grid.RowDefinitions.Count; i++)
+            {
+                for(int j =0;j< grid.ColumnDefinitions.Count; j++)
+                {
+                    BORD = new Border()
+                    {
+                        BorderThickness = new Thickness(1),
+                        CornerRadius = new CornerRadius(5, 5, 5, 5),
+                        BorderBrush = Brushes.Black,
+                        Height = 17.5,
+                        Width = 17.5,
+
+                    };
+                    BORD.Background= (Brush)(new BrushConverter().ConvertFrom(colors[k]));
+                    k++;
+                    grid.Children.Add(BORD);
+                    Grid.SetColumn(BORD, j);
+                    Grid.SetRow(BORD, i);
+                }
+            }
+
+            grid.MouseLeftButtonDown += pickColor;
+
           
         }
+        private void pickColor(Object sender , MouseButtonEventArgs e)
+        {
+            if(e.Source is Border)
+            {
+                if(hit != null ) hit.BorderBrush = Brushes.Black;
+                hit = ((Border)e.Source);
+                pickedColor = hit.Background;
+                hit.BorderBrush = Brushes.White;
+                //MessageBox.Show(String.Format("Picked color is{0}", pickedColor));
+            }
+        }
+
+        
     }
 }
