78b3ac2c262b37373c3563c1ecc61003da7800b5
IoRaa <ka_dandani@esi.dz>
2022-04-05 16:32:33 +0100

    more maintainabiity

--- ColorPicker.xaml.cs
+++ ColorPicker.xaml.cs
@@ -33,9 +33,9 @@ namespace Projet2Cp
             Border BORD;
             int k = 0; 
             String[] colors  =new string[20] {"#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"};
-            for(int i = 0; i < grid.RowDefinitions.Count; i++)
+            for(int i = 0; i < grid.Rows; i++)
             {
-                for(int j =0;j< grid.ColumnDefinitions.Count; j++)
+                for(int j =0;j< grid.Columns; j++)
                 {
                     BORD = new Border()
                     {
