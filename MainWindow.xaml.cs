9af86dfed6faf69784cc07f13188682948a4c365
IoRaa <ka_dandani@esi.dz>
2022-04-06 20:35:08 +0100

    Merged branches (younes / main ) (partially)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,15 +19,38 @@ namespace Projet2Cp
 
     public partial class MainWindow : Window
     {
-        double gridStep = 30;
-        private List<ShapePair> shapes;
-        private Point mousePosition; // to save the mouse position when needed 
-        private double actualPoint;
-        private PointCollection drawingPoints;
+     
+       
         Point centrePoly = new Point();
-        Line axeSym;
-        Ellipse centreSym; 
-        
+        Line axeSym; // l'axe de symetrie selectionné
+        Ellipse centreSym; // le centre de symetrie 
+
+
+
+
+
+        private List<ShapePair> shapePairs; //represents the pairs of shapes
+        private Point mousePosition; //mouse position in canvas
+        private Point actualPoint;//actual point to draw in 
+        private PointCollection drawingPoints;
+        private double step; // the step of the grid 
+        private PointCollection shapepc, shapeSym; // the current shape being drawn  , and a holder for his symetrix 
+
+        private ShapePair currentShapePair;
+        private Polygon polygone;
+        private Polyline polyline;
+        private Ellipse ellipse;// to represent a point in the canvas
+        private Line line;
+
+
+
+
+        private bool isDrawing, isRotating = false;
+        private bool isOverShape = false;
+        private bool isTransforming = false;
+        private bool isOrigin = true;
+
+        Point clickPosition = new Point(); //always indicates where the mouse "left-clicked" on the canvas
 
 
 
@@ -35,39 +58,409 @@ namespace Projet2Cp
         public MainWindow()
         {
             InitializeComponent();
+
             toolBar.addPolygon.Click += addPolygon;
             toolBar.horiz.Click += updateAxe;
             toolBar.verti.Click += updateAxe;
             toolBar.diag1.Click += updateAxe;
             toolBar.diag2.Click += updateAxe;
             toolBar.centre.Click += updateAxe;
-            centreSym = new Ellipse()
-            {
-                Height = 10,
-                Width = 10,
-                Fill = toolBar.CP.pickedColorRempli,
-                Stroke = toolBar.CP.pickedColorTrace,
-                
+            toolBar.effacerTout.Click += effacerTout;
 
-            };
-            axeSym = new Line();
-            axeSym.StrokeThickness = 1;
-            axeSym.Stroke = toolBar.CP.pickedColorTrace;
-            axeSym.X1 = canvas.ActualWidth / 2;
-            axeSym.Y1 = 0.5;
-            axeSym.X2 = canvas.ActualWidth / 2;
-            axeSym.Y2 = canvas.ActualHeight;
-            canvas.Children.Add(axeSym);
-            if (axeSym == null) MessageBox.Show("hh");
-           
+
+            //initialiser le centre de symetrie 
+            if (centreSym == null)
+            {
+                centreSym = new Ellipse()
+                {
+                    Height = 10,
+                    Width = 10,
+                    Fill = toolBar.CP.pickedColorRempli,
+                    Stroke = toolBar.CP.pickedColorTrace,
+                };
+            }
+
+
+
+            step = 25;
+            isDrawing = true; //for now the only option available
+            //canvas.MouseMove += rotating;
+            canvas.MouseMove += mouseCursor;
+            // canvas.MouseLeftButtonDown += hitShape;
+            canvas.MouseLeave += ellipseCleaner;
+            canvas.MouseMove += currentMousePosition;
+            canvas.MouseMove += translating;
+            initCanvas();
+
+
+
 
 
 
         }
+
+        //=======================================================================================================//
+        // CurrentMousePosition : continuously updates "mousePosition" in canvas while not clicking on anything  //
+        //=======================================================================================================//
+
+        void currentMousePosition(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                mousePosition = e.GetPosition(canvas);
+        }
+
+        //=======================================================================================================//
+        //                                          INIT_CANVAS
+        //=======================================================================================================//
+
+        private void initCanvas()
+        {
+            shapePairs = new List<ShapePair>();
+
+            shapepc = new PointCollection();
+
+            polyline = new Polyline()
+            {
+                Stroke = toolBar.CP.pickedColorTrace,
+                StrokeThickness = 1,
+                Points = shapepc,
+            };
+
+            canvas.Children.Add(polyline);
+
+            line = new Line()
+            {
+                Stroke = toolBar.CP.pickedColorTrace,
+                StrokeThickness = 1,
+            };
+
+            canvas.Children.Add(line);
+            uncheckAxes();
+
+
+
+
+        }
+
+      
+
+        //====================================================================================================================//
+        //  TRANSLATING: if we are drawing + leftButton pressed + we are on a shape, we translate the shape or its corner     //  
+        //====================================================================================================================//
+
+
+
+        private void translating(Object sender, MouseEventArgs e) //v1 : sans correction du symetrique
+        {
+            if (isTransforming && (e.LeftButton == MouseButtonState.Pressed) && isDrawing)
+            {
+                Vector vec = e.GetPosition(canvas) - mousePosition;
+                mousePosition = e.GetPosition(canvas); //on reactulise clickPosition pour le prochain move
+                currentShapePair.translating(vec, isOrigin);
+            }
+
+        
+        }
+
+        //====================================================================================================================//
+        //                          ELLIPSE_CLEANER : ENLEVE LA PREVIEW ELLIPSE UNE FOIS QU'ON QUITTE LE CANVAS               //  
+        //====================================================================================================================//
+
+
+        private void ellipseCleaner(Object sender, MouseEventArgs e)
+        {
+            canvas.Children.Remove(ellipse);
+        }
+
+        //====================================================================================================================//
+        //                          CANVAS_MLBD : COORDINE LE TOUS !! (DETERMINE CE QU'IL FAUT FAIRE AU CLICK)                //  
+        //====================================================================================================================//
+
+        private void canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            clickPosition = e.GetPosition(canvas); //On recupere la position du click a toute fin utiles ...
+                                                   
+            if (isDrawing)
+            {
+                if (!isOverShape)
+                    polyline.Points.Add(actualPoint);
+                else if (e.Source is Shape)
+                {
+                    isTransforming = true;
+                    bool stop = false;
+                    int i;
+                    for (i = 0; i < shapePairs.Count && !stop; i++)
+                    {
+                        stop = (shapePairs[i].origin == e.Source || shapePairs[i].sym == e.Source);
+                    }
+
+                    if (stop) //hiya nrmlm bla ma nverifyi ...
+                    {
+                        i--;
+                        currentShapePair = shapePairs[i];
+                        isOrigin = (shapePairs[i].origin == e.Source);
+                    }
+                }
+
+            }
+        }
+
+        //====================================================================================================================//
+        //                          CANVAS_MLBU : COORDINE LE TOUS !! (DETERMINE CE QU'IL FAUT FAIRE AU DE_CLICK)             //  
+        //====================================================================================================================//
+
+        private void canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isTransforming = false;
+        }
+
+        //====================================================================================================================//
+        //                CANVAS_MOUSE_MOVE : DESSINE LA PREVIEW ELLIPSE SI : DRAWING + NOT_OVER_SHAPE + NOT_TRANSOFRMING     //  
+        //====================================================================================================================//
+
+
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing && !isOverShape && !isTransforming)
+            {
+                //Update the color incase it's changes 
+                if (line != null) line.Stroke = toolBar.CP.pickedColorTrace;
+                if(polyline!=null) polyline.Stroke = toolBar.CP.pickedColorTrace;
+
+                canvas.Children.Remove(ellipse);
+                mousePosition = e.GetPosition(canvas);
+
+                actualPoint.X = Math.Round(mousePosition.X / step) * step;
+                actualPoint.Y = Math.Round(mousePosition.Y / step) * step;
+
+                if (Math.Abs(actualPoint.X - mousePosition.X) < 12.5 && Math.Abs(actualPoint.Y - mousePosition.Y) < 12.5 && canvas.IsMouseOver)
+                {// if the cursor is close enough to an intersection then we draw a point 
+                    ellipse = new Ellipse
+
+                    {
+                        Width = 10,
+                        Height = 10,
+                        Fill = Brushes.Black,
+                    };
+                    ellipse.MouseLeftButtonDown += canvas_MouseLeftButtonDown; // C'EST PROVISOIR !!
+                    canvas.Children.Add(ellipse);
+                    Canvas.SetLeft(ellipse, actualPoint.X - 5);
+                    Canvas.SetTop(ellipse, actualPoint.Y - 5);
+                }
+
+                //---------------------------------------------------------------------------------------//
+
+                if (polyline.Points.Count > 0 && canvas.IsMouseOver) // ie il a engagé un dessin
+                {
+
+                    line.X1 = polyline.Points[polyline.Points.Count - 1].X;
+                    line.Y1 = polyline.Points[polyline.Points.Count - 1].Y;
+
+                    line.X2 = actualPoint.X;
+                    line.Y2 = actualPoint.Y;
+
+                }
+            }
+
+        }
+
+        //====================================================================================================================//
+        // SHAPE_MOUSE_ENTER : NETTOIE LA PREVIEW ELLIPSE ET LA BLOCK (AVEC SHAP_OVER = TRUE) DES QUE SHAPE_ENTER             //  
+        //====================================================================================================================//
+
+        public void shapeMouseEnter(object sender, MouseEventArgs e) //maybe its bad practice ....
+        {
+            if (isOverShape == false)
+            {
+                isOverShape = true;
+                canvas.Children.Remove(ellipse);
+            }
+        }
+
+        //====================================================================================================================//
+        //                  SHAPE_MOUSE_LEAVE: DEBLOCK PREVIEW ELLIPSE (AVEC SHAPE_OVER = FALSE)                              //
+        //====================================================================================================================//
+        public void shapeMouseLeave(object sender, MouseEventArgs e)
+        {
+            isOverShape = false;
+        }
+
+        //==================================================================================================================//
+        //       CANVAS_MRBD: DESSINE LE SHAPE CONTENU DANS POLYLINE                                                        //
+        //==================================================================================================================//
+
+        private void canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            // creating the shape 
+            if (shapepc.Count > 1) //if the PointCollection of the polyline contains at least two points
+            {
+                if (Point.Equals(polyline.Points[0], polyline.Points[polyline.Points.Count - 1])) //if the shape is closed
+                {
+                    polygone = new Polygon()
+                    {
+                        Points = shapepc,
+                        Fill = toolBar.CP.pickedColorRempli,
+                        Stroke = toolBar.CP.pickedColorTrace,
+                        StrokeThickness = 3,
+                    };
+
+                    polygone.MouseEnter += shapeMouseEnter;
+                    polygone.MouseLeave += shapeMouseLeave;
+                    canvas.Children.Remove(polyline); //we remove the preview polyline
+                    shapePairs.Add(new ShapePair(polygone, canvas, shapeMouseEnter, shapeMouseLeave)); //we create a new shapePair with origin = polygon "linked to canvas"
+                }
+
+                else
+                {
+                    //we keep the polyline
+                    polyline.StrokeThickness = 8;
+                    polyline.MouseMove += shapeMouseEnter;
+                    polyline.MouseLeave += shapeMouseLeave;
+                    shapePairs.Add(new ShapePair(polyline, canvas, shapeMouseEnter, shapeMouseLeave));
+                }
+            }
+
+            //We clean the mess
+
+            shapepc = new PointCollection();
+
+            polyline = new Polyline()
+            {
+                Stroke = toolBar.CP.pickedColorTrace,
+                StrokeThickness = 1,
+                Points = shapepc,
+            };
+
+            canvas.Children.Add(polyline);
+
+
+            canvas.Children.Remove(line); //we remove the last line from the canvas
+
+            line = new Line()
+            {
+                Stroke = toolBar.CP.pickedColorTrace,
+                StrokeThickness = 1,
+            };
+
+            canvas.Children.Add(line); //we add the new empty line...GarbageCollector use 100 ! XDDD
+
+        }
+
+
+        //==================================================================================================================//
+        //       MOUSE_CURSOR: UPDATES THE CURSOR STYLE (AT EACH MOUSE_MOVE) DEPENDING ON THE SITUATION)                     //
+        //==================================================================================================================//
+        private void mouseCursor(Object sender, MouseEventArgs e)
+        {
+            if (isRotating)
+            {
+
+                if (e.Source is Shape) this.Cursor = Cursors.Hand;
+                else this.Cursor = Cursors.Arrow;
+            }
+            if (isDrawing)
+            {
+                if (isOverShape)
+                    this.Cursor = Cursors.Hand;
+                else
+                    this.Cursor = Cursors.Pen;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void effacerTout(object sender, RoutedEventArgs e)
+        {
+            //reset
+            canvas.Children.Clear();
+            initCanvas();
+
+        }
+
+        private void drawPoint(Point p, SolidColorBrush c)
+        {
+            ellipse = new Ellipse
+            {
+                Width = 10,
+                Height = 10,
+                Fill = c,
+            };
+
+
+            canvas.Children.Add(ellipse);
+            Canvas.SetLeft(ellipse, actualPoint.X - 5);
+
+            Canvas.SetTop(ellipse, actualPoint.Y - 5);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void uncheckAxes()
+        {
+            toolBar.horiz.IsChecked = toolBar.verti.IsChecked = toolBar.diag1.IsChecked = toolBar.diag2.IsChecked = toolBar.centre.IsChecked = false;
+            canvas.Children.Remove(axeSym);
+            axeSym = null;
+
+        }
+
         private void updateAxe(Object sender, RoutedEventArgs e)
         {
-            axeSym.Stroke = toolBar.CP.pickedColorTrace;
+            
             canvas.Children.Remove(centreSym);
+
+
+            if (axeSym == null)
+            {
+                axeSym = new Line();
+                axeSym.StrokeThickness = 1;
+                axeSym.Stroke = toolBar.CP.pickedColorTrace;
+                axeSym.X1 = canvas.ActualWidth / 2;
+                axeSym.Y1 = 0.5;
+                axeSym.X2 = canvas.ActualWidth / 2;
+                axeSym.Y2 = canvas.ActualHeight;
+                canvas.Children.Add(axeSym);
+            }
+            axeSym.Stroke = toolBar.CP.pickedColorTrace;
             switch (true)
             {
                 case true when (bool) toolBar.horiz.IsChecked:
@@ -139,10 +532,7 @@ namespace Projet2Cp
             }
         }
 
-            private void DrawPolygon(Object sender, MouseEventArgs e)
-        {
-            mousePosition= e.GetPosition(canvas);
-        }
+     
         private void addPolygon(Object sender, RoutedEventArgs e)
         {
             centrePoly.X=(double)canvas.ActualWidth/(double)2;
@@ -154,7 +544,7 @@ namespace Projet2Cp
                 Stroke = toolBar.CP.pickedColorTrace,
 
             };
-            Point fstPt = new Point(centrePoly.X + toolBar.Rayon*gridStep, centrePoly.Y);
+            Point fstPt = new Point(centrePoly.X + toolBar.Rayon*step, centrePoly.Y);
             poly.Points.Add(fstPt);
             RotateTransform trans;
 
@@ -171,7 +561,10 @@ namespace Projet2Cp
                 poly.Points.Add(trans.Transform(fstPt));
                
             }
-            canvas.Children.Add(poly);
+
+            poly.MouseEnter += shapeMouseEnter;
+            poly.MouseLeave += shapeMouseLeave;
+            shapePairs.Add(new ShapePair(poly, canvas, shapeMouseEnter, shapeMouseLeave));
 
 
 
--- ShapePair.cs
+++ ShapePair.cs
@@ -4,13 +4,233 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Projet2Cp
 {
     internal class ShapePair
     {
-        Shape origin;
-        Shape sym;
+        Canvas canvas;
+        public Shape origin { get; set; }
+        private List<Ellipse> oEllipse = new List<Ellipse>();
 
+        private Line sline ; //will represent the current line of symmetry between the two shapes
+        private Point spoint; //Will represent the current point of symmetry between the two shapes
+        public Shape sym { get; set; }
+        private List<Ellipse> sEllipse = new List<Ellipse>();
+
+        //============================================================================================================================//
+        //                                              TESTING CONSTRUCOR                                                            //
+        //============================================================================================================================//
+
+        public ShapePair(Shape origin,Canvas canvas,MouseEventHandler shapeMouseEnter, MouseEventHandler shapeMouseLeave)
+        {
+            this.canvas = canvas;
+            this.origin = origin;
+            if(origin is Polygon)
+                canvas.Children.Add(origin);
+            drawEllipses(true);
+
+            //------------------------test
+           /* sline = new Line()
+            {
+                X1 = 400,
+                Y1 = 0,
+                X2 = 400,
+                Y2 = 300,
+                Stroke = Brushes.Blue,
+                StrokeThickness = 3,
+            };
+
+            canvas.Children.Add(sline);
+
+            symGen(shapeMouseEnter, shapeMouseLeave);*/
+
+        }
+
+        //============================================================================================================================//
+        //                                              drawingOrigin                                                                 //
+        //============================================================================================================================//
+
+        private void drawEllipses(bool isOrigin) //draws the ellipseCollection of either origin or sym on canvas
+        {
+                PointCollection shapepc;
+                
+                if(isOrigin)
+                {
+                    for (int i = 0; i < oEllipse.Count; i++)
+                        canvas.Children.Remove(oEllipse[i]);
+                
+                    oEllipse = new List<Ellipse>();
+                
+                    if (origin is Polygon)
+                        shapepc = ((Polygon)origin).Points;
+                    else
+                        shapepc = ((Polyline)origin).Points;
+
+                }
+                
+                else
+                {
+                    for (int i = 0; i < sEllipse.Count; i++)
+                        canvas.Children.Remove(sEllipse[i]);
+
+                    sEllipse = new List<Ellipse>();
+
+                    if (sym is Polygon)
+                        shapepc = ((Polygon)sym).Points;
+                    else
+                        shapepc = ((Polyline)sym).Points;
+
+                }
+
+                //-------------------------------
+                Ellipse ellipse;
+
+                for(int i = 0; i<shapepc.Count; i++)
+                {
+                    ellipse = new Ellipse()
+                    {
+                        Height = 10,
+                        Width = 10,
+                        Fill = Brushes.White,
+                        Stroke = Brushes.Black,
+
+                    };
+
+                    if (isOrigin)
+                        oEllipse.Add(ellipse);
+                    else
+                        sEllipse.Add(ellipse);
+                    
+                    canvas.Children.Add(ellipse);
+                    Canvas.SetLeft(ellipse, shapepc[i].X - 5);
+                    Canvas.SetTop(ellipse, shapepc[i].Y - 5);
+                }
+
+        }
+    
+
+
+
+    //============================================================================================================================//
+    //                                                          translating                                                       //
+    //============================================================================================================================//
+
+        public void translating(Vector vec, bool isOrigin)
+        {
+           
+            PointCollection shapepc;
+            
+            if (origin is Polygon)
+                    shapepc = ((Polygon)origin).Points;
+            else
+                    shapepc = ((Polyline)origin).Points;
+            
+            for (int i = 0; i < shapepc.Count; i++)
+                shapepc[i] = shapepc[i] + vec;
+
+            drawEllipses(isOrigin);
+          
+        }
+
+
+
+        //============================================================================================================================//
+        //                                                          SYM_GEN                                                           //
+        //============================================================================================================================//
+
+
+
+        public void symGen(MouseEventHandler shapeMouseEnter, MouseEventHandler shapeMouseLeave)
+        {
+            PointCollection shapepc;
+            PointCollection symmetricpc = new PointCollection(); //symmetric point collection
+
+            Point p1 = new Point(sline.X1, sline.Y1);
+            Point p2 = new Point(sline.X2, sline.Y2);
+
+            Shape old;
+            Shape newer;
+
+            if (sym == null) //donc orgin != null c'est lui le old
+                old = origin;
+            else
+                old = sym;
+
+                //On recupere la PointCollection de old et on instantie newer selon la nature de old
+                if (old is Polygon)
+                { 
+                    shapepc = ((Polygon)old).Points;
+                    newer = new Polygon()
+                    {
+                        Stroke = Brushes.Black, //((Polygon)old).Stroke, //j'ai de gros doute sur cette manoeuvr ....
+                        StrokeThickness = 3,//((Polygon)old).StrokeThickness,
+                        Fill = Brushes.Red,//((Polygon)old).Fill,
+                    };
+                    
+                    ((Polygon)newer).Points = symmetricpc;
+                }
+                else
+                {
+                    shapepc = ((Polyline)old).Points;
+                    newer = new Polyline()
+                    {
+                        Stroke = Brushes.Black,//((Polyline)old).Stroke, //j'ai de gros doute sur cette manoeuvr ....
+                        StrokeThickness = 3,//((Polyline)old).StrokeThickness,
+                       // Fill = Brushes.Red,//((Polyline)old).Fill,
+
+                    };
+                    
+                    ((Polyline)newer).Points = symmetricpc;
+
+                }
+
+                for(int i = 0; i<shapepc.Count; i++)
+                    symmetricpc.Add( symPoint( p1, p2, shapepc[i] ) );
+
+            newer.MouseEnter += shapeMouseEnter;
+            newer.MouseLeave += shapeMouseLeave;
+
+
+            canvas.Children.Add(newer);
+            
+            if (sym == null)
+            {
+                sym = newer;
+                drawEllipses(false);
+            }
+            else
+            {
+                origin = newer;
+                drawEllipses(true);
+
+            }
+
+
+        }
+
+        private double distancePointPoint(Point firstPoint, Point secondPoint)
+        {
+            return Math.Sqrt(Math.Pow(secondPoint.X - firstPoint.X, 2) + Math.Pow(secondPoint.Y - firstPoint.Y, 2));
+        }
+
+
+    //=================================================================================================================//
+    //                                                  AxialSymGen                                                    //
+    //=================================================================================================================//
+
+    private Point symPoint(Point p1, Point p2, Point p3)
+    {
+        double k = ((p2.Y - p1.Y) * (p3.X - p1.X) - (p2.X - p1.X) * (p3.Y - p1.Y)) / (Math.Pow(p2.X - p1.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        return new Point( 2 * ( p3.X - k * (p2.Y-p1.Y) ) - p3.X , 2 * ( p3.Y + k*(p2.X - p1.X) ) - p3.Y );
     }
-}
+
+
+    }//END OF CLASS
+
+}//END OF NAMESPACE
+
